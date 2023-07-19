487d2daf01723d4ed0bb78956874f18232f0bcef
Mukkara Mukesh <mukkara.m@tvsnext.com>
2023-07-19 14:47:34 +0530

    [Mukesh] Add : created for data inventory management

---
+++ OOPs/InventoryDetailsOperation.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOPs
+{
+    internal class InventoryDetailsOperation
+    {
+        public void ReadInventoryJson(String filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            List<InventoryDetails> list = JsonConvert.DeserializeObject<List<InventoryDetails>>(json);
+            foreach (var data in list)
+            {
+                Console.WriteLine(data.Name + "data.Weight" + " " + data.PricePerKg);
+            }
+        }
+    }
+}
---
+++ OOPs/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+
+namespace OOPs
+{
+    public class program
+    {
+        static string inventory_filePath = @"D:\Problem statemets\OOPs\OOPs\OOPs\InventoryData.json";
+        public static void Main(string[] args)
+        {
+            InventoryDetailsOperation details = new InventoryDetailsOperation();
+            details.ReadInventoryJson(inventory_filePath);
+        }
+    }
+}
+
